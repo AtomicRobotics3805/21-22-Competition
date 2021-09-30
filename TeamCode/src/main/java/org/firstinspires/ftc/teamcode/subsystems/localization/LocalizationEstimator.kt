73d583a77d6e861a02f213f5512851f53c633318
AdamMeilicke <meiliada000@edmonds15.org>
2021-09-30 12:37:55 -0700

    Added wpilib to the TeamCode gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/localization/LocalizationEstimator.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/localization/LocalizationEstimator.kt
@@ -1,7 +1,19 @@
 package org.firstinspires.ftc.teamcode.subsystems.localization
 
-
-
+import com.acmerobotics.roadrunner.geometry.Pose2d as RoadRunnerPose2d
+import com.acmerobotics.roadrunner.localization.Localizer
+import edu.wpi.first.wpilibj.estimator.ExtendedKalmanFilter
+import edu.wpi.first.wpilibj.estimator.KalmanFilterLatencyCompensator
+import edu.wpi.first.wpilibj.geometry.Pose2d
+import edu.wpi.first.wpilibj.geometry.Rotation2d
+import edu.wpi.first.wpilibj.math.Discretization
+import edu.wpi.first.wpilibj.math.StateSpaceUtil
+import edu.wpi.first.wpiutil.math.Matrix
+import edu.wpi.first.wpiutil.math.Nat
+import edu.wpi.first.wpiutil.math.VecBuilder
+import edu.wpi.first.wpiutil.math.numbers.*
+import java.util.function.BiConsumer
+import java.util.function.BiFunction
 
 
 /**
@@ -25,10 +37,10 @@ object LocalizationEstimator : Localizer {
     private lateinit var gyroOffset: Rotation2d
     private lateinit var previousAngle: Rotation2d
     fun initialize(
-            gyroAngle: Rotation2d, initialPoseMeters: Pose2d,
-            stateStdDevs: Matrix<N5, N1>,
-            localMeasurementStdDevs: Matrix<N6, N1>, visionMeasurementStdDevs: Matrix<N3, N1>,
-            nominalDtSeconds: Double
+        gyroAngle: Rotation2d, initialPoseMeters: Pose2d,
+        stateStdDevs: Matrix<N5, N1>,
+        localMeasurementStdDevs: Matrix<N6, N1>, visionMeasurementStdDevs: Matrix<N3, N1>,
+        nominalDtSeconds: Double
     ) {
         nominalDt = nominalDtSeconds
 
