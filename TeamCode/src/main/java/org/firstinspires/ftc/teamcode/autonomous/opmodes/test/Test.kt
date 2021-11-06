cd199ff4840ef3a3d1d5cddded840171815e4527
AdamMeilicke <meiliada000@edmonds15.org>
2021-11-05 19:38:29 -0700

    Deleted MecanumDrive test programs because they are no longer necessary

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/opmodes/test/Test.kt
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.autonomous.opmodes.test
-
-class Test {
-    companion object {
-        var test = 3
-    }
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/opmodes/test/TestObject.kt
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.autonomous.opmodes.test
-
-import org.firstinspires.ftc.teamcode.subsystems.driving.MecanumDrive
-
-object TestObject {
-    var drive = MecanumDrive
-    var test = 5
-}
