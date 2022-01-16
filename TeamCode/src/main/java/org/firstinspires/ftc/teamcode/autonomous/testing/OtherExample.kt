fe94c3aa2285847943daa6d152d43db1a0d46f18
Adam Meilicke <meiliada000@edmonds15.org>
2022-01-16 11:08:57 -0800

    Deleted unnecessary test programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/testing/OtherExample.kt
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.autonomous.testing
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-
-class OtherExample : LinearOpMode() {
-    override fun runOpMode() {
-
-    }
-}
