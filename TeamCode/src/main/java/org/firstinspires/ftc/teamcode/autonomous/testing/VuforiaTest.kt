b391e7dfb94f9bbecea2bffffbc5774fd8b9ad1f
Adam Meilicke <meiliada000@edmonds15.org>
2022-01-16 11:19:08 -0800

    Made VuforiaTest a real command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/testing/VuforiaTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/testing/VuforiaTest.kt
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.autonomous.testing
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.Constants.opMode
 import org.firstinspires.ftc.teamcode.subsystems.localization.VuforiaLocalizer
 
+@TeleOp
 class VuforiaTest : LinearOpMode() {
     override fun runOpMode() {
         opMode = this
