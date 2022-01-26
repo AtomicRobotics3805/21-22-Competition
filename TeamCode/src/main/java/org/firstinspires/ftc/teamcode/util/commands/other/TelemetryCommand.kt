0fdb7e2acaf9dcced07cd5a2d86d90d30e72e7f1
AdamMeilicke <meiliada000@edmonds15.org>
2022-01-26 10:25:58 -0800

    Added the option for TelemetryCommand messages to be lambdas that return strings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/commands/other/TelemetryCommand.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/commands/other/TelemetryCommand.kt
@@ -4,18 +4,23 @@ import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.Constants
 import org.firstinspires.ftc.teamcode.util.commands.AtomicCommand
 
-class TelemetryCommand(private val time: Double, private val message: String) : AtomicCommand() {
+class TelemetryCommand(private val time: Double, private val message: () -> String) : AtomicCommand() {
     override val _isDone: Boolean
         get() = timer.seconds() > time
 
-    constructor(time: Double, caption: String, data: String) : this(time, "$caption: $data")
+    constructor(time: Double, caption: String, data: () -> String) :
+            this(time, { caption + ": " + data.invoke() })
+    constructor(time: Double, message: String) :
+            this(time, { message })
+    constructor(time: Double, caption: String, data: String) :
+            this(time, { "$caption: $data" })
 
     override fun start() {
         timer.reset()
     }
 
     override fun execute() {
-        Constants.opMode.telemetry.addLine(message)
+        Constants.opMode.telemetry.addLine(message.invoke())
     }
 
     val timer = ElapsedTime()
